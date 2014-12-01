835a86c042269e03cde1cfe7608a74465ed1d045
Finnur Torfason <finnurtorfa@gmail.com>
2014-12-01 12:53:20 +0100

    Implement the factorial function

--- ch4/factorial.s
+++ ch4/factorial.s
@@ -8,6 +8,13 @@
 
 .globl _start
 _start:
+  pushl $4        # The factorial number
+  call factorial
+  addl $4, %esp   # Move the stack pointer back
+
+  movl %eax, %ebx # Accordind to C calling convention return value is stored in
+                  # %eax. Linux however dictates that status code be stored in
+                  # %ebx.
   movl $1, %eax   # The exit() system call
   int $0x80
 
@@ -21,8 +28,26 @@ _start:
 #
 # VARIABLES:  
 #             %ebx - The factorial number
-#             -4(%ebp) - The current result
 #             %eax - Temporary storage
 .type factorial, @function
 factorial: 
-  ret                 # Return from the function
+  pushl %ebp            # Save old base pointer
+  movl %esp, %ebp       # Make stack pointer the base pointer
+  movl 8(%ebp), %eax    # Put the factorial number in %eax
+
+  cmpl $1, %eax         # Check if we have reached 1
+  je factorial_cleanup  # Clean up if so
+
+  decl %eax             # Decrement the factorial number
+  pushl %eax            # Push the new factorial number on the stack
+  call factorial        # Call the factorial function with decremented factorial
+                        # number
+  movl 8(%ebp), %ebx    # Reload parameter into %ebx
+
+  imull %ebx, %eax      # Multiply the result with the factorial number and
+                        # store in %eax
+
+factorial_cleanup:
+  movl %ebp, %esp  # Restore the stack pointer
+  popl %ebp        # Restore the base pointer
+  ret              # Return from the function
