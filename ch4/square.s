6edce6daf072c41eec6165f4c7a6e8be1bbdaafc
Finnur Torfason <finnurtorfa@gmail.com>
2014-12-03 21:54:26 +0100

    Finish the square function

--- ch4/square.s
+++ ch4/square.s
@@ -8,6 +8,13 @@
 
 .globl _start
 _start:
+  pushl $4        # The input number
+  call square
+  addl $4, %esp   # Move the stack pointer back
+
+  movl %eax, %ebx # Accordind to C calling convention return value is stored in
+                  # %eax. Linux however dictates that status code be stored in
+                  # %ebx.
   movl $1, %eax   # The exit() system call
   int $0x80
 
@@ -21,6 +28,14 @@ _start:
 #             %ebx - The square
 .type square, @function
 square: 
+  pushl %ebp            # Save old base pointer
+  movl %esp, %ebp       # Make stack pointer the base pointer
+  movl 8(%ebp), %eax    # Put the input number in %eax
+
+  imull %eax, %eax      # Multiply the input number with itself and store in
+                        # %eax
+
+square_cleanup:
   movl %ebp, %esp  # Restore the stack pointer
   popl %ebp        # Restore the base pointer
   ret              # Return from the function
