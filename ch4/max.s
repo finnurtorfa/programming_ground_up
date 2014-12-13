a9cdac56a9420dea03db79f881dfc6326a7ed0f6
Finnur Torfason <finnurtorfa@gmail.com>
2014-12-13 11:28:21 +0100

    Implement and test the maximum function

--- ch4/max.s
+++ ch4/max.s
@@ -14,8 +14,25 @@ list3:
 .section .text 
 .globl _start
 _start: 
-  movl $1, %eax     # 1 stands for the exit() system call
-  int $0x80         # This wakes up the kernel to run the exit command
+  pushl $list1    # Push the address of the beginning of list1
+                  # onto the stack. It will by the argument.
+  call maximum 
+  addl $4, %esp   # Move the stack pointer back
+
+  pushl $list2    # Push the address of the beginning of list1
+                  # onto the stack. It will by the argument.
+  call maximum 
+  addl $4, %esp   # Move the stack pointer back
+
+  pushl $list3    # Push the address of the beginning of list1
+                  # onto the stack. It will by the argument.
+  call maximum 
+  addl $4, %esp   # Move the stack pointer back
+
+  movl %eax, %ebx # The first item is the current largest
+                  # item
+  movl $1, %eax   # 1 stands for the exit() system call
+  int $0x80       # This wakes up the kernel to run the exit command
 
 # PURPOSE:    A function to find the largest element in a list
 #
@@ -24,7 +41,29 @@ _start:
 # OUTPUT:     The maximum value of the list
 #
 # VARIABLES:  
-#             %ebx - The maximum number
-#             %eax - Temporary storage
+#             %ebx - Pointer to the address of the list
+#             %eax - Maximum number
 .type maximum, @function
 maximum: 
+  pushl %ebp          # Save old base pointer(return address)
+  movl %esp, %ebp     # Make stack pointer the base pointer
+  
+  movl 8(%ebp), %ebx  # Put pointer address in %ebx
+  movl (%ebx), %eax   # Put the data pointed to by the address in %ebx in %eax
+
+maximum_loop:
+  cmpl $0, (%ebx)
+  je maximum_exit
+
+  addl $4, %ebx       # Add 4 to the address, so it points to the next item
+
+  cmpl %eax, (%ebx)   # Compare the current largest value to the data pointed to
+                      # by the address in %ebx
+  jle maximum_loop    # Jump to beginning of loop if it isn't larger
+  movl (%ebx), %eax   # Move the larger value into %eax
+  jmp maximum_loop    # Jump to beginning of loop
+
+maximum_exit:
+  movl %ebp, %esp     # Restore the stack pointer
+  popl %ebp           # Restore the base pointer
+  ret
