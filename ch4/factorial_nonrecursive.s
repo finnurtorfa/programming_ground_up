cccf7878cbc5ecfcf38eafb7f892718ea168c25b
Finnur Torfason <finnurtorfa@gmail.com>
2014-12-14 22:49:26 +0100

    Implement the factorial function non recursively

--- ch4/factorial_nonrecursive.s
+++ ch4/factorial_nonrecursive.s
@@ -8,6 +8,15 @@
 
 .globl _start
 _start:
+  pushl $5        # The factorial number
+  call factorial
+  addl $4, %esp   # Move the stack pointer back
+
+  movl %eax, %ebx # Accordind to C calling convention return value is stored in
+                  # %eax. Linux however dictates that status code be stored in
+                  # %ebx.
+  movl $1, %eax   # The exit() system call
+  int $0x80
 
 # PURPOSE:    This function is used to calculate the factorial of x. I.e. x!
 #
@@ -24,6 +33,19 @@ _start:
 factorial: 
   pushl %ebp            # Save old base pointer
   movl %esp, %ebp       # Make stack pointer the base pointer
+  movl 8(%ebp), %eax    # Put the factorial number in %eax
+  movl %eax, %ebx       # Put the factorial number in %ebx
+
+factorial_loop:
+  cmpl $1, %ebx         # Check if we have reached 1
+  je factorial_cleanup  # Clean up if so
+
+  decl %ebx             # Decrement the factorial number
+  imull %ebx, %eax      # Multiply the decremented factorial number with the
+                        # current result and store in %eax
+  jmp factorial_loop
+
+factorial_cleanup:
   movl %ebp, %esp  # Restore the stack pointer
   popl %ebp        # Restore the base pointer
   ret              # Return from the function
