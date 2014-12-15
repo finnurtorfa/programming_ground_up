55a20019cf738bf9ea948041bb68fd7b026c9542
Finnur Torfason <finnurtorfa@gmail.com>
2014-12-15 20:33:49 +0100

    Add a file with some standard definitions, such as syscall numbers and file descriptors

---
+++ stddefs.s
@@ -0,0 +1,14 @@
+# Linux syscalls numbers definitions
+.equ SYS_EXIT,  1
+.equ SYS_READ,  2
+.equ SYS_WRITE, 3
+.equ SYS_OPEN,  4
+.equ SYS_CLOSE, 5
+.equ SYS_BRK,   6
+.equ SYS_IRQ,   0x80
+
+# Standard File Descriptors
+.equ STDIN,  0
+.equ STDOUT, 1
+.equ STDERR, 2
+
